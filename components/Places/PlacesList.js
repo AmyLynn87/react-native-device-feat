27dace21099f4e1c33e33f6c25ecb32888a167dd
Elisa Crescentini <elisa@ruggedsoftware.dev>
2023-11-04 11:36:56 +0100

    styling a bit

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,5 +1,6 @@
 //Libs
 import { FlatList, StyleSheet, Text, View } from "react-native";
+import { Colors } from "../../constants/colors";
 
 //Local
 
@@ -33,5 +34,6 @@ const styles = StyleSheet.create({
   },
   fallbackText: {
     fontSize: 16,
+    color: Colors.primary200,
   },
 });
